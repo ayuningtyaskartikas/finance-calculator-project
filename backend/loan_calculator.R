eb100d14fba3ab84fbe298d27b06dbc3be9246e5
jaera <jaera.vllr@gmail.com>
2026-06-22 20:38:12 -1000

    update calculator for user input

--- backend/loan_calculator.R
+++ backend/loan_calculator.R
@@ -1,10 +1,6 @@
 calculate_monthly_payment <- function(principal, annual_rate, years) {
-    #convert the annual % into a monthly decimal
     r <- annual_rate / 12 / 100
-
-    #monthly payments
     n <- years * 12
-
     M <- principal * r * (1 + r)^n / ((1 + r)^n - 1)
 
     return(round (M,2))
@@ -18,11 +14,6 @@ principal_vec <- c()
 interest_vec <- c()
 balance_vec <- c()
 
-# define inputs
-principal <- 200000
-annual_rate <- 6.5
-years <- 30
-
 # calculate the values for the loop
 r <- annual_rate / 12 / 100
 n <- years * 12
@@ -31,22 +22,13 @@ balance <- principal   #updated each month
 
 #loop 
 for (i in 1:n) {
-
-  # 1. calculate interest for this month
     interest_portion <- balance * r
-  # 2. calculate principal for this month
     principal_portion <- M - interest_portion
-  # 3. update balance (subtract principal)
     balance <- balance - principal_portion
-  # 4. append i to months_vec
     months_vec <- c(months_vec, i) 
-  # 5. append payment to payment_vec
     payment_vec <- c(payment_vec, M)
-  # 6. append principal to principal_vec
     principal_vec <- c(principal_vec, principal_portion)
-  # 7. append interest to interest_vec
     interest_vec <- c(interest_vec, round(interest_portion, 2))
-  # 8. append balance to balance_vec
     balance_vec <- c(balance_vec, round(balance, 2))
 }
 
